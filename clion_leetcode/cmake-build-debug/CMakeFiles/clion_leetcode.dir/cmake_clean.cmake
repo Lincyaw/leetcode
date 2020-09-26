07bde42933230888fa8854e8d1472170f2451834
Fay <814750204@qq.com>
2020-09-26 11:42:01 +0800

    test

---
+++ clion_leetcode/cmake-build-debug/CMakeFiles/clion_leetcode.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clion_leetcode.dir/offer_51_reversed_pairs.cpp.obj"
+  "CMakeFiles/clion_leetcode.dir/template.cpp.obj"
+  "clion_leetcode.exe"
+  "clion_leetcode.exe.manifest"
+  "clion_leetcode.lib"
+  "clion_leetcode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clion_leetcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
